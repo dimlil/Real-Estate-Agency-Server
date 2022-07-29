8161f47dcdaee270de75676583e4a43631ea449a
dimlil <d.v.vassilev@gmail.com>
2022-07-29 16:24:42 +0300

    feat/ added route for authentication

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import Express from "express";
 import dotenv from 'dotenv'
 import router from "./routes/index.js";
+import authRouter from "./routes/authRoute.js";
 import { connectToDB } from "./connectToDB.js";
 
 const app = Express();
 dotenv.config();
 
-app.use(router)
+app.use(router);
+app.use(authRouter);
+
 const port=process.env.PORT
 connectToDB()
 
