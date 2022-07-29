b9207b686fc1fa35875a4b36a7e1f747fda670b3
dimlil <d.v.vassilev@gmail.com>
2022-07-29 16:02:01 +0300

    feat/ added basic router

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import Express from "express";
 import dotenv from 'dotenv'
+import router from "./routes/index.js";
 
 const app = Express();
 dotenv.config();
+
+app.use(router)
 const port=process.env.PORT
 
 app.listen(port, console.log(`Server is running on port: ${port}`));
