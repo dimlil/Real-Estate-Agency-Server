375edd5d45a9b5df09efeba96c73263dcee036fb
dimlil <d.v.vassilev@gmail.com>
2022-07-29 16:10:12 +0300

    feat/ added mongoDB

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import Express from "express";
 import dotenv from 'dotenv'
 import router from "./routes/index.js";
+import { connectToDB } from "./connectToDB.js";
 
 const app = Express();
 dotenv.config();
 
 app.use(router)
 const port=process.env.PORT
+connectToDB()
 
 app.listen(port, console.log(`Server is running on port: ${port}`));
