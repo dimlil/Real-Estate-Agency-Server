7cfc13e2e7f716d661fcea6af7aba3e4392dd6e1
dimlil <d.v.vassilev@gmail.com>
2022-07-29 15:55:53 +0300

    feat/ added env

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import Express from "express";
+import dotenv from 'dotenv'
 
 const app = Express();
-const port=4000;
+dotenv.config();
+const port=process.env.PORT
 
 app.listen(port, console.log(`Server is running on port: ${port}`));
