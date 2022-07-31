3726e9b8969a6076d7178c5dc120591a70640e4c
dimlil <d.v.vassilev@gmail.com>
2022-07-31 16:18:26 +0300

    feat/ added verifying token

--- controllers/verifyUser.js
+++ controllers/verifyUser.js
@@ -4,3 +4,11 @@ export const generateToken = data => {
     const token = jwt.sign(data, process.env.PRIVATE_KEY);
     return token;
 }
+export const verifyUser = token =>{
+    try {
+        jwt.verify(token,process.env.PRIVATE_KEY);
+        return true
+    } catch (error) {
+        return false
+    }
+}
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import { register } from "../controllers/auth.js";
+import { verifyUser } from "../controllers/verifyUser.js";
 const authRouter = Router();
 
 authRouter.post('/register', (req, res) => {
-    register(req,res);
-})
+    register(req, res);
+});
+
+authRouter.get('/verify', async (req, res) => {
+    const token = req.cookies['aid'];
+    const result = await verifyUser(token);
+    res.send(result)
+});
 
 export default authRouter
