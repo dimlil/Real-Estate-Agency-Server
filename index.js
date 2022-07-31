607e6554e76cd63a421fd355c947dedea6e789a8
dimlil <d.v.vassilev@gmail.com>
2022-07-31 16:07:19 +0300

    feat/ added cors

--- index.js
+++ index.js
@@ -4,19 +4,22 @@ import router from "./routes/index.js";
 import authRouter from "./routes/authRoute.js";
 import { connectToDB } from "./connectToDB.js";
 import cookieParser from "cookie-parser";
+import cors from "cors"
 
 const app = Express();
+dotenv.config();
+
+app.use(cors({ credentials: true, origin: process.env.ORIGIN_URL }))
 
 app.use(Express.json());
-app.use(Express.urlencoded({ extended: true}));
+app.use(Express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-dotenv.config();
 
 app.use(router);
 app.use(authRouter);
 
-const port=process.env.PORT
+const port = process.env.PORT
 connectToDB()
 
 app.listen(port, console.log(`Server is running on port: ${port}`));
