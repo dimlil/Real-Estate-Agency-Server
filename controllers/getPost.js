9acaf8185e34ca0ac603b17db013949474c23a5d
dimlil <d.v.vassilev@gmail.com>
2022-08-05 12:28:05 +0300

    feat/ gething post details

--- controllers/getPost.js
+++ controllers/getPost.js
@@ -1,26 +1,36 @@
 import House from "../models/HouseModel.js"
 
-export const getAllPosts=async(req,res)=>{
+export const getAllPosts = async (req, res) => {
     try {
         const houses = await House.find({});
-        if (houses.length===0) {
+        if (houses.length === 0) {
             throw 'Nothing found'
         }
         res.json(houses);
     } catch (error) {
-        res.status(404).json({error})
+        res.status(404).json({ error })
     }
 }
 
-export const getTopPosts=async(req,res)=>{
+export const getTopPosts = async (req, res) => {
     try {
-        const houses = await House.find({}).sort({createdAt: 'desc'}).limit(3);
-        //reverse the db
-        if (houses.length===0) {
+        const houses = await House.find({}).sort({ createdAt: 'desc' }).limit(3);
+        if (houses.length === 0) {
             throw 'Nothing found'
         }
         res.json(houses);
     } catch (error) {
-        res.status(404).json({error})
+        res.status(404).json({ error })
+    }
+}
+
+export const getPost = async (req, res) => {
+    try {
+        const house = await House.findById(req.params.id);
+        const isLoggedIn = req.cookies['userId'] ? true : false;
+        const isOwner = house.Owner._id.toHexString() === req.cookies['userId'];
+        res.status(200).json({ house, isLoggedIn, isOwner, });
+    } catch (error) {
+        res.status(404).json(error.message);
     }
 }
