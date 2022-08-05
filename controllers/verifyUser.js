8db44d6a017e35048416c89ec3ad3ef17f1cfcc7
dimlil <d.v.vassilev@gmail.com>
2022-08-05 12:26:40 +0300

    fix/ format

--- controllers/verifyUser.js
+++ controllers/verifyUser.js
@@ -4,9 +4,9 @@ export const generateToken = data => {
     const token = jwt.sign(data, process.env.PRIVATE_KEY);
     return token;
 }
-export const checkToken = token =>{
+export const checkToken = token => {
     try {
-        jwt.verify(token,process.env.PRIVATE_KEY);
+        jwt.verify(token, process.env.PRIVATE_KEY);
         return true
     } catch (error) {
         return false
